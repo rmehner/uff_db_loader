2be8228ff38c6d07cfe7c2712f128ed8b583dde4
Robin Mehner <robin@coding-robin.de>
2022-07-20 17:03:29 +0200

    Escape the $ properly, or else the password won't be passed properly

--- lib/uff_db_loader/mysql.rb
+++ lib/uff_db_loader/mysql.rb
@@ -3,7 +3,7 @@
 module UffDbLoader
   module Mysql
     def self.dump_command_template
-      "ssh %user%@%host% \"docker exec -i #{APP_NAME}_%environment%_db sh -c 'exec mysqldump --opt --no-tablespaces -uroot -p\"\$MYSQL_ROOT_PASSWORD\" %database%'\" > %target%"
+      "ssh %user%@%host% \"docker exec -i #{APP_NAME}_%environment%_db sh -c 'exec mysqldump --opt --no-tablespaces -uroot -p\"\\$MYSQL_ROOT_PASSWORD\" %database%'\" > %target%"
     end
 
     def self.restore_command(database_name, result_file_path)
